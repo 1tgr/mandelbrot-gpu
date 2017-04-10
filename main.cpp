c479864baf446c7e08e64b3177a17e7782e7eec1
Tim Robinson <tim.g.robinson@gmail.com>
2017-04-10 20:35:31 +0100

    CUDA

--- main.cpp
+++ main.cpp
@@ -1,27 +1,38 @@
 #include <algorithm>
 #include <math.h>
-#include <tuple>
+#include <stdio.h>
 #include <vector>
+
+#if __NVCC__
+#include "thrust/device_vector.h"
+#include "thrust/host_vector.h"
+#define DEVICE __device__
+#else
+#define DEVICE
+#endif
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb/stb_image_write.h"
 
-static std::tuple<double, double> image_to_viewport(
-        int image_width, int image_height, std::tuple<int, int> image_pt,
-        std::tuple<double, double> viewport_tl, std::tuple<double, double> viewport_br) {
-    int image_x, image_y;
-    std::tie(image_x, image_y) = image_pt;
-    double viewport_left, viewport_top, viewport_right, viewport_bottom;
-    std::tie(viewport_left, viewport_top) = viewport_tl;
-    std::tie(viewport_right, viewport_bottom) = viewport_br;
+static DEVICE void image_to_viewport(
+    int image_width,
+    int image_height,
+    int image_x,
+    int image_y,
+    double viewport_left,
+    double viewport_top,
+    double viewport_right,
+    double viewport_bottom,
+    double& viewport_x,
+    double& viewport_y
+) {
     auto viewport_width = viewport_right - viewport_left;
     auto viewport_height = viewport_bottom - viewport_top;
-    return std::make_tuple(
-        (static_cast<double>(image_x) * viewport_width) / static_cast<double>(image_width) + viewport_left,
-        (static_cast<double>(image_y) * viewport_height) / static_cast<double>(image_height) + viewport_top);
+    viewport_x = (static_cast<double>(image_x) * viewport_width) / static_cast<double>(image_width) + viewport_left;
+    viewport_y = (static_cast<double>(image_y) * viewport_height) / static_cast<double>(image_height) + viewport_top;
 }
 
-static void mandel(char *image, int image_width, int image_height, int image_x, int image_y) {
+template<typename T> static DEVICE void mandel(T& image, int image_width, int image_height, int image_x, int image_y) {
     /*
      *    -2.5    +1.0
      * -1.0 +-------+
@@ -32,7 +43,7 @@ static void mandel(char *image, int image_width, int image_height, int image_x, 
      */
 
     double x0, y0;
-    std::tie(x0, y0) = image_to_viewport(image_width, image_height, std::make_tuple(image_x, image_y), std::make_tuple(-2.5, -1.0), std::make_tuple(1.0, 1.0));
+    image_to_viewport(image_width, image_height, image_x, image_y, -2.5, -1.0, 1.0, 1.0, x0, y0);
 
     auto x = 0.0, y = 0.0;
     auto iteration = 0, max_iteration = 1000;
@@ -45,7 +56,7 @@ static void mandel(char *image, int image_width, int image_height, int image_x, 
 
     if (iteration < max_iteration) {
         auto log_zn = log(x * x + y * y) / 2;
-        auto nu = log(log_zn / log(2)) / log(2);
+        auto nu = log(log_zn / log(2.0)) / log(2.0);
         iteration += 1 - nu;
     }
 
@@ -55,20 +66,40 @@ static void mandel(char *image, int image_width, int image_height, int image_x, 
     auto color = linear_interpolate(color1, color2, iteration % 1);
     plot(Px, Py, color);
     */
-    auto image_ptr = image + (image_y * image_width + image_x) * 3;
-    image_ptr[0] = image_ptr[1] = image_ptr[2] = iteration;
+    auto offset = (image_y * image_width + image_x) * 3;
+    image[offset] = image[offset + 1] = image[offset + 2] = iteration;
 }
 
+#ifdef __NVCC__
+static __global__ void mandel_kernel(char *image, int width, int height) {
+    auto x = blockIdx.x * blockDim.x + threadIdx.x;
+    auto y = blockIdx.y * blockDim.y + threadIdx.y;
+    if (x < width && y < height) {
+        mandel(image, width, height, x, y);
+    }
+}
+#endif
 
 int main() {
     const auto width = 640;
     const auto height = 480;
     auto image = std::vector<char>(width * height * 3);
+
+#ifdef __NVCC__
+    printf("Using GPU\n");
+    dim3 blockDim(16, 16, 1);
+    dim3 gridDim((width + blockDim.x - 1) / blockDim.x, (height + blockDim.y - 1) / blockDim.y, 1);
+    auto device_image = thrust::device_vector<char>(image.capacity());
+    mandel_kernel<<<gridDim, blockDim, 0>>>(thrust::raw_pointer_cast(&device_image[0]), width, height);
+    thrust::copy(device_image.begin(), device_image.end(), image.begin());
+#else
+    printf("Using CPU\n");
     for (auto y = 0; y < height; y++) {
         for (auto x = 0; x < width; x++) {
-            mandel(&image[0], width, height, x, y);
+            mandel(image, width, height, x, y);
         }
     }
+#endif
 
     stbi_write_png("image.png", width, height, 3, &image[0], width * 3);
     return 0;
